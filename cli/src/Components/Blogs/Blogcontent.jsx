9ac28353c56e3a283d9d4dcedda07aca23ae6101
luv730236 <122589399+luv730236@users.noreply.github.com>
2024-02-21 18:10:06 +0530

    xs
    sxxs

--- cli/src/Components/Blogs/Blogcontent.jsx
+++ cli/src/Components/Blogs/Blogcontent.jsx
@@ -3,92 +3,92 @@ import React from 'react'
 function Blogcontent() {
   return (
     <div>
-  <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16 relative">
-    <div className="bg-cover bg-center text-center overflow-hidden min-h-500 bg-cover bg-center custom-bg "
-        
-        title="Woman holding a mug">
+ <main className="mt-10">
+
+<div className="mb-4 md:mb-0 w-full max-w-screen-md mx-auto relative h-24">
+  <div className="absolute left-0 bottom-0 w-full h-full z-10 blogcontent-img"
+   ></div>
+  <img src="https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80" class="absolute left-0 top-0 w-full h-full z-0 object-cover" />
+  <div className="p-4 absolute bottom-0 left-0 z-20">
+    <a href="#"
+      className="px-4 py-1 bg-black text-gray-200 inline-flex items-center justify-center mb-2">Nutrition</a>
+    <h2 className="text-4xl font-semibold text-gray-100 leading-tight">
+      Pellentesque a consectetur velit, ac molestie ipsum. Donec sodales, massa et auctor.
+    </h2>
+    <div className="flex mt-3">
+      <img src="https://randomuser.me/api/portraits/men/97.jpg"
+        className="h-10 w-10 rounded-full mr-2 object-cover" />
+      <div>
+        <p className="font-semibold text-gray-200 text-sm"> Mike Sullivan </p>
+        <p className="font-semibold text-gray-400 text-xs"> 14 Aug </p>
+      </div>
     </div>
-    <div className="max-w-3xl mx-auto">
-        <div
-            className="mt-3 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
-            <div className="bg-white relative top-0 -mt-32 p-5 sm:p-10">
-                <h1 href="#" className="text-gray-900 font-bold text-3xl mb-2">Revenge of the Never Trumpers</h1>
-                <p className="text-gray-700 text-xs mt-2">Written By:
-                    <a href="#"
-                        className="text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
-                        Ahmad Sultani
-                    </a> In
-                    <a href="#"
-                        className="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
-                        Election
-                    </a>,
-                    <a href="#"
-                        className="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
-                        Politics
-                    </a>
-
-                </p>
-
-                <p className="text-base leading-8 my-5">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-                    and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
-                    leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-                    with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-                    publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-
-                <h3 className="text-2xl font-bold my-5">#1. What is Lorem Ipsum?</h3>
-
-                <p className="text-base leading-8 my-5">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-                    and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
-                    leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-                    with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-                    publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-
-                <blockquote className="border-l-4 text-base italic leading-8 my-5 p-5 text-indigo-600">Lorem Ipsum is simply
-                    dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
-                    dummy text ever since the 1500s
-                </blockquote>
-
-                <p className="text-base leading-8 my-5">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-                    and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
-                    leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-                    with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-                    publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-
-
-                <a href="#"
-                    className="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
-                    #Election
-                </a>,
-                <a href="#"
-                    className="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
-                    #people
-                </a>,
-                <a href="#"
-                    className="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
-                    #Election2020
-                </a>,
-                <a href="#"
-                    className="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
-                    #trump
-                </a>,<a href="#"
-                    className="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
-                    #Joe
-                </a>
-
-            </div>
-
-        </div>
-    </div>
+  </div>
 </div>
+
+<div className="px-4 lg:px-0 mt-12 text-gray-700 max-w-screen-md mx-auto text-lg leading-relaxed">
+  <p className="pb-6">Advantage old had otherwise sincerity dependent additions. It in adapted natural hastily is
+    justice. Six draw
+    you him full not mean evil. Prepare garrets it expense windows shewing do an. She projection advantages
+    resolution son indulgence. Part sure on no long life am at ever. In songs above he as drawn to. Gay was
+    outlived peculiar rendered led six.</p>
+
+  <p className="pb-6">Difficulty on insensible reasonable in. From as went he they. Preference themselves me as
+    thoroughly
+    partiality considered on in estimating. Middletons acceptance discovered projecting so is so or. In or
+    attachment inquietude remarkably comparison at an. Is surrounded prosperous stimulated am me discretion
+    expression. But truth being state can she china widow. Occasional preference fat remarkably now projecting
+    uncommonly dissimilar. Sentiments projection particular companions interested do at my delightful. Listening
+    newspaper in advantage frankness to concluded unwilling.</p>
+
+  <p className="pb-6">Adieus except say barton put feebly favour him. Entreaties unpleasant sufficient few pianoforte
+    discovered
+    uncommonly ask. Morning cousins amongst in mr weather do neither. Warmth object matter course active law
+    spring six. Pursuit showing tedious unknown winding see had man add. And park eyes too more him. Simple excuse
+    active had son wholly coming number add. Though all excuse ladies rather regard assure yet. If feelings so
+    prospect no as raptures quitting.</p>
+
+  <div className="border-l-4 border-gray-500 pl-4 mb-6 italic rounded">
+    Sportsman do offending supported extremity breakfast by listening. Decisively advantages nor
+    expression
+    unpleasing she led met. Estate was tended ten boy nearer seemed. As so seeing latter he should thirty whence.
+    Steepest speaking up attended it as. Made neat an on be gave show snug tore.
+  </div>
+
+  <p className="pb-6">Exquisite cordially mr happiness of neglected distrusts. Boisterous impossible unaffected he me
+    everything.
+    Is fine loud deal an rent open give. Find upon and sent spot song son eyes. Do endeavor he differed carriage
+    is learning my graceful. Feel plan know is he like on pure. See burst found sir met think hopes are marry
+    among. Delightful remarkably new assistance saw literature mrs favourable.</p>
+
+  <h2 className="text-2xl text-gray-800 font-semibold mb-4 mt-4">Uneasy barton seeing remark happen his has</h2>
+
+  <p className="pb-6">Guest it he tears aware as. Make my no cold of need. He been past in by my hard. Warmly thrown
+    oh he common
+    future. Otherwise concealed favourite frankness on be at dashwoods defective at. Sympathize interested
+    simplicity at do projecting increasing terminated. As edward settle limits at in.</p>
+
+  <p className="pb-6">Dashwood contempt on mr unlocked resolved provided of of. Stanhill wondered it it welcomed oh.
+    Hundred no
+    prudent he however smiling at an offence. If earnestly extremity he he propriety something admitting convinced
+    ye. Pleasant in to although as if differed horrible. Mirth his quick its set front enjoy hoped had there. Who
+    connection imprudence middletons too but increasing celebrated principles joy. Herself too improve gay winding
+    ask expense are compact. New all paid few hard pure she.</p>
+
+  <p className="pb-6">Breakfast agreeable incommode departure it an. By ignorant at on wondered relation. Enough at
+    tastes really
+    so cousin am of. Extensive therefore supported by extremity of contented. Is pursuit compact demesne invited
+    elderly be. View him she roof tell her case has sigh. Moreover is possible he admitted sociable concerns. By
+    in cold no less been sent hard hill.</p>
+
+  <p className="pb-6">Detract yet delight written farther his general. If in so bred at dare rose lose good. Feel and
+    make two real
+    miss use easy. Celebrated delightful an especially increasing instrument am. Indulgence contrasted sufficient
+    to unpleasant in in insensible favourable. Latter remark hunted enough vulgar say man. Sitting hearted on it
+    without me.</p>
+
+</div>
+</main>
     </div>
   )
 }
