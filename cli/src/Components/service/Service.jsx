58677daa1879e6e8382afbdd116d8ea27afd7ded
rudrakshgroup12 <168619227+rudrakshgroup12@users.noreply.github.com>
2024-05-06 17:31:43 +0530

    Update Service.jsx

--- cli/src/Components/service/Service.jsx
+++ cli/src/Components/service/Service.jsx
@@ -1,6 +1,6 @@
 import React ,{useState} from 'react'
 import { Link } from "react-router-dom";
-import "./Service.css";
+import "./service.css";
 import dialkroservice from "../assets/dialkroservice.png";
 const Service = () => {
 
