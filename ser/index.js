2a0356e8d39fb1d44a9ac8cf0a1008bb34e6aef8
dishu009 <147999734+dishu009@users.noreply.github.com>
2024-05-15 11:12:13 +0530

    change cors

--- ser/index.js
+++ ser/index.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 // app.use(cors())
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5174"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
