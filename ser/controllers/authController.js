69a0312311ad3db535f551faf00999e01a13c702
dishu009 <147999734+dishu009@users.noreply.github.com>
2024-08-08 17:41:51 +0530

    delete file

--- ser/controllers/authController.js
+++ ser/controllers/authController.js
@@ -60,58 +60,7 @@ export const registerController = async (req, res) => {
   //   console.log(`route is working`);
 };
 
-// export const registerController = async (req, res) => {
-//   try {
-//     const { username, email, password, phone, address, answer } = req.body;
-//     //validations
-//     if (!username) {
-//       return res.send({ message: "Username is required!" });
-//     }
-//     if (!email) {
-//       return res.send({ message: "Email is required!" });
-//     }
-//     if (!password) {
-//       return res.send({ message: "Password is required!" });
-//     }
-//     if (!phone) {
-//       return res.send({ message: "Phone is required!" });
-//     }
-//     if (!address) {
-//       return res.send({ message: "Address is required!" });
-//     }
-//     if (!answer) {
-//       return res.send({ message: "Answer is required!" });
-//     }
 
-//     const existingUser = await userModel.findOne({ email });
-//     if (existingUser) {
-//       return res.status(200).send({
-//         success: false,
-//         message: "Already Register please login",
-//       });
-//     }
-//     const hashedPassword = await hashPassword(password);
-//     const user = await new userModel({
-//       username,
-//       email,
-//       phone,
-//       address,
-//       password: hashedPassword,
-//       answer,
-//     }).save();
-//     res.status(201).send({
-//       success: true,
-//       message: "User Registered successfully",
-//       user,
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send({
-//       success: false,
-//       message: "Error in registration",
-//     });
-//   }
-// };
 
 //Post Login
 export default { registerController };
